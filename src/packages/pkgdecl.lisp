bcf7bd05cfd503e62a512b4b33affeefda43e283
youngde <youngde>
2000-10-10 15:26:27 +0000

    Added a few basic things

---
+++ src/packages/pkgdecl.lisp
@@ -0,0 +1,11 @@
+;;;
+;;; File: pkgdecl.lisp
+;;; Description: Package declarations for LISA.
+;;;
+;;; $Id: pkgdecl.lisp,v 1.1 2000/10/10 15:26:27 youngde Exp $
+
+(in-package "CL-USER")
+
+(defpackage "LISA"
+  (:use "COMMON-LISP"))
+
