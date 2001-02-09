065101378d0d80f82081ce78093988af375abc34
youngde <youngde>
2001-02-09 15:19:56 +0000

    snapshot

--- src/engine/token-tree.lisp
+++ src/engine/token-tree.lisp
@@ -20,7 +20,7 @@
 ;;; File: token-tree.lisp
 ;;; Description: Maintains a hashed collection of tokens.
 
-;;; $Id: token-tree.lisp,v 1.13 2001/02/09 01:49:59 youngde Exp $
+;;; $Id: token-tree.lisp,v 1.14 2001/02/09 15:19:56 youngde Exp $
 
 (in-package :lisa)
 
@@ -29,7 +29,9 @@
           :accessor get-table)
    (use-sortcode-p :initarg :use-sortcode
                    :initform nil
-                   :accessor get-use-sortcode))
+                   :accessor get-use-sortcode)
+   (size :initform 0
+         :accessor get-size))
   (:documentation
    "Maintains a hashed collection of tokens."))
 
@@ -39,19 +41,30 @@
          (table (get-table self))
          (tokens (gethash hash table)))
     (setf (gethash hash table)
-          (nconc tokens `(,token)))))
+          (push token tokens))
+    (incf (get-size self))
+    (values)))
 
-(defmethod remove-token ((self token-tree) (tok token))
+(defun remove-token (self token)
+  (declare (type token-tree self) (type token token))
   (with-accessors ((table get-table)) self
-    (let* ((key (create-hash-code self tok))
-           (obj (gethash key table)))
-      (unless (null obj)
-        (remhash key table))
-      (values obj))))
+    (let* ((key (create-hash-code self token))
+           (token-list (gethash key table))
+           (foundp nil))
+      (setf token-list
+            (delete-if #'(lambda (obj)
+                           (setf foundp (equals obj token)))
+                       token-list :count 1))
+      (when foundp
+        (if (null token-list)
+            (remhash key table)
+          (setf (gethash key table) token-list))
+        (decf (get-size self)))
+      (values foundp))))
 
-(defun use-sortcode-p (tree)
-  (declare (type token-tree tree))
-  (get-use-sortcode tree))
+(defun use-sortcode-p (self)
+  (declare (type token-tree self))
+  (get-use-sortcode self))
 
 (defun create-hash-code (tree token)
   (declare (type token-tree tree) (type token token))
@@ -59,12 +72,16 @@
       (hash-code token)
     (get-fact-id (get-top-fact token))))
 
-(defmethod clear-tree ((self token-tree))
-  (clrhash (get-table self)))
+(defun clear-tree (self)
+  (declare (type token-tree self))
+  (clrhash (get-table self))
+  (setf (get-size self) 0))
 
-(defmethod token-tree-count ((self token-tree))
-  (hash-table-count (get-table self)))
+(defun token-tree-count (self)
+  (declare (type token-tree self))
+  (get-size self))
 
+#+ignore
 (defmacro with-tree-iterator ((key value tree) &body body)
   "Iterates over each element in TREE (an instance of TOKEN-TREE) and
   evaluates BODY. RETURN may be used to exit from the iterator with
@@ -77,6 +94,28 @@
              (,generator)
            (if ,foundp
                (progn ,@body)
+             (return nil)))))))
+
+(defmacro with-tree-iterator ((key value tree) &body body)
+  "Iterates over each element in TREE (an instance of TOKEN-TREE) and
+  evaluates BODY. RETURN may be used to exit from the iterator with
+  specified results."
+  (let ((generator (gensym))
+        (foundp (gensym))
+        (token-list (gensym))
+        (token (gensym))
+        (rval (gensym)))
+    `(with-hash-table-iterator (,generator (get-table ,tree))
+       (loop
+         (multiple-value-bind (,foundp ,key ,token-list)
+             (,generator)
+           (if ,foundp
+               (let ((,rval
+                      (dolist (,token ,token-list)
+                        (setf ,value ,token)
+                        ,@body)))
+                 (when ,rval
+                   (return ,rval)))
              (return nil)))))))
 
 (defun make-token-tree (&key (use-sortcode-p nil))
