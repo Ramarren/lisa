d801012405d0152ef0394cdac3aa8d876668a7ca
youngde <youngde>
2002-04-03 03:35:59 +0000

    Resuming work on queries

--- src/query/retrieve.lisp
+++ src/query/retrieve.lisp
@@ -21,7 +21,7 @@
 ;;; Description: Macros and functions implementing LISA's early attempt at a
 ;;; query language.
 
-;;; $Id: retrieve.lisp,v 1.6 2002/01/10 16:52:15 youngde Exp $
+;;; $Id: retrieve.lisp,v 1.7 2002/04/03 03:36:00 youngde Exp $
 
 (in-package "LISA")
 
@@ -33,6 +33,12 @@
 ;;; time a query is issued, borrowing the FACT table from CURRENT-ENGINE in
 ;;; the process. This is not a permanent solution, but it will remain in place
 ;;; while I gather feedback on the overall query mechanism.
+
+(defun run-query (query)
+  (let ((?name (get-name query)))
+    (assert (query-fact (name ?name)))
+    (run)
+    (values *query-result*)))
 
 (defun define-and-run-query (name body)
   (let ((*query-result* '())
@@ -49,6 +55,24 @@
             (if (consp ',name) ,name ',name)))
        (define-and-run-query ,rule-name ',body))))
 
+#+ignore
+(defun define-and-run-query (name body)
+  (let ((*query-result* '())
+        (query-engine (make-inference-engine)))
+    (setf (get-facts query-engine) (get-facts (current-engine)))
+    (with-inference-engine (query-engine)
+      (add-rule query-engine (define-rule name body))
+      (run)
+      (values *query-result*))))
+
+#+ignore
+(defmacro defquery (name &body body)
+  (let ((rule-name (gensym)))
+    `(let ((,rule-name
+            (if (consp ',name) ,name ',name)))
+       (define-and-run-query ,rule-name ',body))))
+
+#+ignore
 (defmacro retrieve ((&rest varlist) &body body)
   (flet ((make-query-binding (var)
            `(cons ',var (instance-of-shadow-fact ,var))))
@@ -56,3 +80,33 @@
        ,@body
        =>
        (push (list ,@(mapcar #'make-query-binding varlist)) *query-result*))))
+
+(defmacro retrieve ((&rest varlist) &body body)
+  (flet ((make-query-binding (var)
+           `(cons ',var (instance-of-shadow-fact ,var))))
+    (let ((query (gensym))
+          (query-name (gensym)))
+      `(let ((,query (find-query ',body)))
+         (if (null ,query)
+             (let ((,query-name (gensym)))
+               (defquery ,query-name
+                   (query-fact (name ,query-name))
+                 ,@body
+                 =>
+                 (push (list ,@(mapcar #'make-query-binding varlist)) *query-result*)))
+           (run-query ,query))))))
+
+(defun normalize-query (body)
+  (let ((varlist '())
+        (index 0))
+    (flet ((map-variable (var)
+             (let ((item (cdr (assoc var varlist))))
+               (when (null item)
+                 (setf item
+                   (make-symbol (format nil "?_~D" (incf index))))
+                 (setf varlist (acons var item varlist)))
+               (values item))))
+      (mapcar #'(lambda (obj)
+                  (if (variablep obj) (map-variable obj) obj))
+              (flatten body)))))
+             
