800a5873f314f20b7baa08e1ecf2c5624cff309a
youngde <youngde>
2002-11-22 20:05:04 +0000

    *** empty log message ***

--- misc/metaclass.lisp
+++ misc/metaclass.lisp
@@ -20,6 +20,32 @@
 ;;; File: metaclass.lisp
 ;;; Description:
 
-;;; $Id: metaclass.lisp,v 1.1 2002/11/22 19:23:12 youngde Exp $
+;;; $Id: metaclass.lisp,v 1.2 2002/11/22 20:05:04 youngde Exp $
 
 (in-package "CL-USER")
+
+(defclass standard-kb-class (standard-class) ())
+
+(defclass standard-kb-object ()
+  ((shared-initialize :initform t))
+  (:metaclass standard-kb-class))
+
+(defmethod shared-initialize :after ((self standard-kb-object) slot-names &rest initargs)
+  (setf (slot-value self 'shared-initialize) nil))
+
+(defmethod (setf slot-value-using-class) :around 
+           (new-value (self standard-kb-class) instance slot)
+  (break)
+  (let ((initializing-p (slot-value instance 'shared-initialize)))
+    (call-next-method)
+    (if initializing-p
+        (format t "Instance ~S during initialization." instance)
+      (format t "Instance ~S setting slot ~S~%" instance slot))
+    new-value))
+
+(defclass frodo (standard-kb-object)
+  ((name :initarg :name
+         :initform nil
+         :accessor frodo-name))
+  (:metaclass standard-kb-class))
+
