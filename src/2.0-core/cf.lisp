849df778bc65fe606f8daa17787dfabcb9d481ab
youngde <youngde>
2004-09-13 15:03:52 +0000

    New file

---
+++ src/2.0-core/cf.lisp
@@ -0,0 +1,39 @@
+;;; This file is part of LISA, the Lisp-based Intelligent Software
+;;; Agents platform.
+
+;;; Copyright (C) 2000 David E. Young
+
+;;; This library is free software; you can redistribute it and/or
+;;; modify it under the terms of the GNU Lesser General Public License
+;;; as published by the Free Software Foundation; either version 2.1
+;;; of the License, or (at your option) any later version.
+
+;;; This library is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU Lesser General Public License for more details.
+
+;;; You should have received a copy of the GNU Lesser General Public License
+;;; along with this library; if not, write to the Free Software
+;;; Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+;;; File: cf.lisp
+;;; Description: Supporting code for Lisa's uncertainty mechanism.
+
+;;; $Id: cf.lisp,v 1.1 2004/09/13 15:03:52 youngde Exp $
+
+(in-package :lisa)
+
+(defun cf-or (a b)
+  (cond ((and (plusp a)
+              (plusp b))
+         (+ a b (* -1 a b)))
+        ((and (minusp a)
+              (minusp b))
+         (+ a b (* a b)))
+        (t (/ (+ a b)
+              (- 1 (min (abs a) (abs b)))))))
+
+(defun cf-and (a b)
+  (min a b))
+
